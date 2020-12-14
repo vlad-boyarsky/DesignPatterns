40b60dd0d85cefeb979e840534252171a7cf535a
hyper-vlados <hypervlados@gmail.com>
2020-12-14 17:41:37 +0200

    AbstractFactory

--- Creational patterns/AbstractFactory/index.php
+++ Creational patterns/AbstractFactory/index.php
@@ -2,9 +2,14 @@
 
 require_once __DIR__ . '/StyleConfigFactory.php';
 
-$colorConfig = (new StyleConfigFactory())->getStyleFactory('light');
-$colorConfig->includeBorderColorStyle()->build();
-$colorConfig->includeBackroundColorStyle()->build();
+$styleConfig = new StyleConfigFactory;
+$modeStyle = $styleConfig->getStyleFactory('dark');
+
+$modeStyle->includeBorderColorStyle()->build();
+$modeStyle->includeBackroundColorStyle()->build();
+
+//$colorConfig->includeBorderColorStyle()->build();
+//$colorConfig->includeBackroundColorStyle()->build();
 
 
 
