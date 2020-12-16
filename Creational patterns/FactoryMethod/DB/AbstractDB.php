c84d1db6483aed3a38a7f3a38c2b7b3774b41ae5
hyper-vlados <hypervlados@gmail.com>
2020-12-16 14:00:20 +0200

    Factory Method

--- Creational patterns/FactoryMethod/DB/AbstractDB.php
+++ Creational patterns/FactoryMethod/DB/AbstractDB.php
@@ -1,7 +1,17 @@
 <?php
 
 
-class AbstractDB
+abstract class AbstractDB
 {
 
+    abstract function getDbConfig();
+
+    public function build()
+    {
+        $dbConfig = $this->getDbConfig();
+        $checkConnection = $dbConfig->check()->run();
+        $connect = $dbConfig->connect()->run();
+
+        return $checkConnection . ' ' . $connect;
+    }
 }
